2d5f381dd4fd8cd2e58d328fff95880722c7276d
Alex Shkor <alex.shkor@gmail.com>
2013-09-06 21:35:07 +0300

    dummy commit

--- src/DQF.Infrastructure/ViewModel/GameViewModel.cs
+++ src/DQF.Infrastructure/ViewModel/GameViewModel.cs
@@ -28,35 +28,4 @@ namespace PAQK.ViewModel
 
         public List<PlayerViewModel> Players { get; set; } 
     }
-
-    public class PlayerViewModel
-    {
-        public int Position { get; set; }
-        public long Cash { get; set; }
-        public long Bid { get; set; }
-        public string UserId { get; set; }
-        public string Name { get; set; }
-        public bool IsMe { get; set; } 
-        public bool CurrentTurn { get; set; } 
-        public List<CardViewModel> Cards { get; set; }
-
-        public PlayerViewModel(PlayerDocument doc, string myUserId)
-        {
-            Position = doc.Position;
-            Cash = doc.Cash;
-            Bid = doc.Bid;
-            UserId = doc.UserId;
-            Name = doc.Name;
-            IsMe = UserId == myUserId;
-            CurrentTurn = doc.CurrentTurn;
-            if (IsMe)
-            {
-                Cards = doc.Cards.Select(x => new CardViewModel(x)).ToList();
-            }
-            else
-            {
-                Cards = new List<CardViewModel>();
-            }
-        }
-    }
 }
