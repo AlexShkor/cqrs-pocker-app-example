4a276ef7950d8b042e393e5b0f08632545ca6842
Egor Tsaryk <egortsaryk@gmail.com>
2014-06-05 12:00:10 +0300

    Raise spinner

--- src/Poker.Web/App/app.js
+++ src/Poker.Web/App/app.js
@@ -7,18 +7,18 @@ var app = angular.module("poker",
      "poker.game"]);
 app.value('$', $);
 app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    
+
     $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/');
-    
+
     $stateProvider
-        .state('new_table', {url:'/tables/create', templateUrl: '/tables/create', controller: 'CreateTableController'})
+        .state('new_table', { url: '/tables/create', templateUrl: '/tables/create', controller: 'CreateTableController' })
         .state('home', { url: '/', templateUrl: '/app/home.html', controller: 'HomeController' })
-        .state('tables', {url: '/tables', templateUrl: '/tables', controller: 'TablesController' })
+        .state('tables', { url: '/tables', templateUrl: '/tables', controller: 'TablesController' })
         .state("game", { url: '/game/view/:tableId', templateUrl: '/game', controller: 'GameController' });
 });
 
-app.controller('AppCtrl', ['$scope', '$rootScope',  "signalsService", "eventAggregatorService", function ($scope, $rootScope,  signalsService, eventAggregatorService) {
+app.controller('AppCtrl', ['$scope', '$rootScope', "signalsService", "eventAggregatorService", function ($scope, $rootScope, signalsService, eventAggregatorService) {
     $scope.init = function (user) {
         signalsService.initialize();
     };
--- src/Poker.Web/App/game.js
+++ src/Poker.Web/App/game.js
@@ -1,5 +1,5 @@
 
-var gameApp = angular.module('poker.game', []);
+var gameApp = angular.module('poker.game', ['rzModule']);
 
 gameApp.controller("GameController", function ($scope, $stateParams, $http, $sce, eventAggregatorService, signalsService) {
     this.$scope = $scope;
@@ -10,13 +10,17 @@ gameApp.controller("GameController", function ($scope, $stateParams, $http, $sce
     signalsService.invoke("connectToTable", $stateParams.tableId);
 
     $scope.RaiseValue = 0;
-    
-    var load = function() {
+
+
+    var load = function () {
         $http.post("/game/load/", { tableId: $stateParams.tableId }).success(function (data) {
             $scope.game = data;
+            $scope.RaiseValue = data.MaxBid;
+
+            initUserRates();
             trustSuitsAsHtml($scope.game);
         });
-    }
+    };
 
     load();
 
@@ -28,7 +32,11 @@ gameApp.controller("GameController", function ($scope, $stateParams, $http, $sce
     };
     $scope.raise = function () {
         if ($scope.RaiseValue > 0) {
-            $http.post("/game/raise", { tableId: $scope.game.Id, amount: $scope.RaiseValue });
+
+            var player = getPlayer($scope.game.CurrentPlayerId);
+            var amount = $scope.RaiseValue - player.Bid;
+
+            $http.post("/game/raise", { tableId: $scope.game.Id, amount: amount });
             $scope.RaiseValue = null;
         }
     };
@@ -40,7 +48,6 @@ gameApp.controller("GameController", function ($scope, $stateParams, $http, $sce
     $scope.refresh = function () {
         load();
     };
-
 
     eventAggregatorService.subscribe("playerTurnChanged", function (e, data) {
 
@@ -71,6 +78,8 @@ gameApp.controller("GameController", function ($scope, $stateParams, $http, $sce
         }
 
         $scope.game.MaxBid = data.MaxBid;
+
+        initUserRates();
         $scope.$apply();
 
     });
@@ -90,6 +99,62 @@ gameApp.controller("GameController", function ($scope, $stateParams, $http, $sce
             card = game.Deck[k];
             card.Symbol = $sce.trustAsHtml(card.Symbol);
         }
+    }
+
+    function getPlayer(id) {
+        for (var i = 0; i < $scope.game.Players.length; i++) {
+            var player = $scope.game.Players[i];
+            for (var j = 0; j < player.Cards.length; j++) {
+                if (player.UserId == id)
+                    return player;
+            }
+        }
+    }
+
+    function initUserRates() {
+
+        var me = getPlayer($scope.game.MyId);
+        var rates = [];
+
+        var availableCash = me.Cash - $scope.game.MaxBid + $scope.game.SmallBlind * 2;
+
+        if (availableCash > 0) {
+
+            var reminder = availableCash % $scope.game.SmallBlind;
+            var loopCashValue = availableCash - reminder;
+            var rate = 0;
+
+            while ($scope.game.SmallBlind < loopCashValue) {
+
+                if (rate == 0) {
+                    rate = $scope.game.MaxBid;
+                    rates.push(rate);
+                } else {
+                    rate += $scope.game.SmallBlind;
+                    rates.push(rate);
+                }
+
+                loopCashValue -= $scope.game.SmallBlind;
+            }
+
+            if (reminder != 0)
+                rates.push(reminder);
+
+            $scope.rate = 0; // index
+            $scope.rateMax = rates.length - 1;
+
+            $scope.minRateDisplay = rates[0];
+            $scope.maxRateDisplay = rates[rates.length - 1];
+
+            $('.bubble').css('display', 'none');
+
+            $scope.currentUserRate = function (rate) {
+                $scope.RaiseValue = rates[rate];
+                return rates[rate].toString();
+            };
+        }
+
+        // TODO: logig if not enougth cash
     }
 
 });
