4cb45253d9b6d4339516d48f42cf4087e6c9d928
Alex Shkor <alex.shkor@gmail.com>
2014-06-09 10:42:16 +0300

    jasmine tests

---
+++ src/Poker.Web/App/Tests/gameTest.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="~/Scripts/jquery-2.0.3.js"/>
+/// <reference path="~/Scripts/angular.js"/>
+/// <reference path="~/Scripts/angular-mocks.js"/>
+/// <reference path="~/App/eventAggregatorService.js"/>
+/// <reference path="~/App/app.js"/>
+/// <reference path="~/App/game.js"/>
+
+describe('Game Controller Test', function () {
+    var ctrl, scope, eventAggregator, http, location;
+
+
+    beforeEach(function () {
+    });
+    beforeEach(inject(['$controller', '$rootScope', 'eventAggregatorService', '$location', '$httpBackend', function ($controller, $rootScope, eventAggregatorService, $location, $httpBackend) {
+        scope = $rootScope.$new();
+        eventAggregator = eventAggregatorService;
+        location = $location;
+        http = $httpBackend;
+
+        ctrl = $controller('GameController', { $scope: scope, $location: location });
+    }]));
+
+    it('should', function () {
+    });
+});
